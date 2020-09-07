66cc0b7ae738f473353f97b2895c534d0ba5919e
Manfred Peter Fackeldey <peter.fackeldey@cern.ch>
2020-09-07 16:47:17 +0000

    Update bootstrap.sh

--- dhi/tasks/bootstrap.sh
+++ dhi/tasks/bootstrap.sh
@@ -4,7 +4,7 @@ action() {
     # on the CERN HTCondor batch, the PATH variable is changed even though "getenv" is set
     # in the job file, so set the PATH manually to the desired same value
     export PATH="{{dhi_env_path}}"
-    # set the HMC_ON_HTCONDOR which is recognized by the setup script below
+    # set the DHI_ON_HTCONDOR which is recognized by the setup script below
     export DHI_ON_HTCONDOR="1"
     echo "Setting up environment"
     # source the main setup
