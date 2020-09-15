1d7df5bc53c4576ea9cb4b9262602436c2417c40
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2020-09-15 12:07:47 +0200

    add tasks for producing prefit postfit shapes && add task for comparing nuisances after fitting

--- dhi/tasks/misc.py
+++ dhi/tasks/misc.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
+import os
 import law
 import luigi
 
 from dhi.tasks.base import CHBase
+from dhi.tasks.nlo.inference import CombDatacards, NLOT2W, ImpactsPulls
 
 
 class ValidateDatacard(CHBase):
@@ -23,4 +25,55 @@ class ValidateDatacard(CHBase):
             mass=self.mass,
             verbosity=self.verbosity,
             out=self.output().path,
+        )
+
+
+class PostFitShapes(CHBase):
+    def requires(self):
+        return NLOT2W.req(self)
+
+    def output(self):
+        return self.local_target("fitDiagnostics.root", store="$DHI_LOCAL_STORE")
+
+    @property
+    def cmd(self):
+        return (
+            "combine -M FitDiagnostics {workspace} -t -1 --expectSignal 1 "
+            "--X-rtd MINIMIZER_analytic -m {mass} -v 2 {params} "
+            "{options} --saveShapes --saveWithUncertainties "
+        ).format(
+            workspace=self.input().path,
+            mass=self.mass,
+            params=ImpactsPulls.params,
+            options=self.stable_options,
+        )
+
+
+class CompareNuisances(CHBase):
+    exe = "$CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py"
+
+    format = luigi.ChoiceParameter(default="html", choices=("html", "latex", "text"))
+
+    def requires(self):
+        return PostFitShapes.req(self)
+
+    def output(self):
+        return self.local_target("nuisances.{}".format(self.ext), store="$DHI_LOCAL_STORE")
+
+    def __init__(self, *args, **kwargs):
+        super(CompareNuisances, self).__init__(*args, **kwargs)
+        if self.format == "html":
+            self.ext = "html"
+        if self.format == "latex":
+            self.ext = "tex"
+        if self.format == "text":
+            self.ext = "txt"
+
+    @property
+    def cmd(self):
+        return "python {exe} -a -f {format} {fitfile} > {output}".format(
+            exe=self.exe,
+            format=self.format,
+            fitfile=self.input().path,
+            output=self.output().path,
         )
