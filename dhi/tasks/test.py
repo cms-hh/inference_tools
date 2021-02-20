06b2deccebd9f47457c8e9e476aa079a1344933b
Marcel R <github.riga@icloud.com>
2021-02-20 10:56:45 +0100

    Refactor test setup.

--- dhi/tasks/test.py
+++ dhi/tasks/test.py
@@ -7,7 +7,9 @@ Task tests based on the $DHI_EXAMPLE_CARDS.
 import os
 from collections import OrderedDict
 
+import law
 import luigi
+import six
 
 from dhi.tasks.base import AnalysisTask, PlotTask, view_output_plots
 from dhi.tasks.limits import (
@@ -28,37 +30,59 @@ from dhi.tasks.studies.model_selection import (
 )
 
 
-class TestPlots(AnalysisTask):
+class TestRegister(law.task.base.Register):
+
+    def __new__(metacls, classname, bases, classdict):
+        # convert test names into "--no-<name>" and "--only-<name>" task parameters
+        for test_name in classdict.get("test_names", []):
+            classdict["no_" + test_name] = luigi.BoolParameter(default=False)
+            classdict["only_" + test_name] = luigi.BoolParameter(default=False)
+
+        return law.task.base.Register.__new__(metacls, classname, bases, classdict)
+
+
+class TestPlots(six.with_metaclass(TestRegister, AnalysisTask)):
 
     task_namespace = "test"
 
-    no_upper_limits = luigi.BoolParameter(default=False)
-    no_multiple_upper_limits = luigi.BoolParameter(default=False)
-    no_multiple_upper_limits_by_model = luigi.BoolParameter(default=False)
-    no_upper_limits_at_point = luigi.BoolParameter(default=False)
-    no_likelihood_scan = luigi.BoolParameter(default=False)
-    no_likelihood_scan_2d = luigi.BoolParameter(default=False)
-    no_multiple_likelihood_scans = luigi.BoolParameter(default=False)
-    no_multiple_likelihood_scans_2d = luigi.BoolParameter(default=False)
-    no_multiple_likelihood_scans_by_model = luigi.BoolParameter(default=False)
-    no_multiple_likelihood_scans_by_model_2d = luigi.BoolParameter(default=False)
-    no_significance_scan = luigi.BoolParameter(default=False)
-    no_multiple_significance_scans = luigi.BoolParameter(default=False)
-    no_pulls_and_impacts = luigi.BoolParameter(default=False)
-    no_exclusion_and_bestfit = luigi.BoolParameter(default=False)
-    no_exclusion_and_bestfit_2d = luigi.BoolParameter(default=False)
-    no_postfit_s_over_b = luigi.BoolParameter(default=False)
-    no_goodness_of_fit = luigi.BoolParameter(default=False)
-    no_multiple_goodness_of_fits = luigi.BoolParameter(default=False)
-    no_morphing_scales = luigi.BoolParameter(default=False)
-    no_morphed_discriminant = luigi.BoolParameter(default=False)
-    no_stat_error_scan = luigi.BoolParameter(default=False)
+    # test names that will to translated to task parameters "--no-<name>" and "--only-<name>"
+    # by the meta class
+    test_names = [
+        "upper_limits",
+        "multiple_upper_limits",
+        "multiple_upper_limits_by_model",
+        "upper_limits_at_point",
+        "likelihood_scan",
+        "likelihood_scan_2d",
+        "multiple_likelihood_scans",
+        "multiple_likelihood_scans_2d",
+        "multiple_likelihood_scans_by_model",
+        "multiple_likelihood_scans_by_model_2d",
+        "significance_scan",
+        "multiple_significance_scans",
+        "pulls_and_impacts",
+        "exclusion_and_bestfit",
+        "exclusion_and_bestfit_2d",
+        "postfit_s_over_b",
+        "goodness_of_fit",
+        "multiple_goodness_of_fits",
+        "morphing_scales",
+        "morphed_discriminant",
+        "stat_error_scan",
+    ]
 
     file_type = PlotTask.file_type
     campaign = PlotTask.campaign
     view_cmd = PlotTask.view_cmd
 
     exclude_params_req = {"view_cmd"}
+
+    def check_enabled(self, test_name):
+        assert(test_name in self.test_names)
+        if any(getattr(self, "only_" + n) for n in self.test_names):
+            return getattr(self, "only_" + test_name)
+        else:
+            return not getattr(self, "no_" + test_name)
 
     def requires(self):
         reqs = OrderedDict()
@@ -69,14 +93,14 @@ class TestPlots(AnalysisTask):
         multi_cards_names = ("ggF", "VBF", "All")
         test_models = tuple("HHModelPinv.model_no_ggf_kl" + kl for kl in "1 2p45 5".split())
 
-        if not self.no_upper_limits:
+        if self.check_enabled("upper_limits"):
             reqs["upper_limits"] = PlotUpperLimits.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_multiple_upper_limits:
+        if self.check_enabled("multiple_upper_limits"):
             reqs["multiple_upper_limits"] = PlotMultipleUpperLimits.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -84,7 +108,7 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_multiple_upper_limits_by_model:
+        if self.check_enabled("multiple_upper_limits_by_model"):
             reqs["multiple_upper_limits_by_model"] = PlotMultipleUpperLimitsByModel.req(self,
                 datacards=ggf_cards,
                 hh_models=test_models,
@@ -92,7 +116,7 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_upper_limits_at_point:
+        if self.check_enabled("upper_limits_at_point"):
             reqs["upper_limits_at_point"] = PlotUpperLimitsAtPoint.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -100,21 +124,21 @@ class TestPlots(AnalysisTask):
                 parameter_values=("kl=1", "kt=1"),
             )
 
-        if not self.no_likelihood_scan:
+        if self.check_enabled("likelihood_scan"):
             reqs["likelihood_scan"] = PlotLikelihoodScan.req(self,
                 datacards=ggf_cards,
                 pois=("kl",),
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_likelihood_scan_2d:
+        if self.check_enabled("likelihood_scan_2d"):
             reqs["likelihood_scan_2d"] = PlotLikelihoodScan.req(self,
                 datacards=ggf_cards,
                 pois=("kl", "kt"),
                 scan_parameters=(("kl", -5., 5.), ("kt", -5., 5.),),
             )
 
-        if not self.no_multiple_likelihood_scans:
+        if self.check_enabled("multiple_likelihood_scans"):
             reqs["multiple_likelihood_scans"] = PlotMultipleLikelihoodScans.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -122,7 +146,7 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_multiple_likelihood_scans_2d:
+        if self.check_enabled("multiple_likelihood_scans_2d"):
             reqs["multiple_likelihood_scans_2d"] = PlotMultipleLikelihoodScans.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -130,7 +154,7 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.), ("kt", -5., 5.),),
             )
 
-        if not self.no_multiple_likelihood_scans_by_model:
+        if self.check_enabled("multiple_likelihood_scans_by_model"):
             reqs["multiple_likelihood_scans_by_model"] = PlotMultipleLikelihoodScansByModel.req(
                 self,
                 datacards=ggf_cards,
@@ -139,7 +163,7 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_multiple_likelihood_scans_by_model_2d:
+        if self.check_enabled("multiple_likelihood_scans_by_model_2d"):
             reqs["multiple_likelihood_scans_by_model_2d"] = PlotMultipleLikelihoodScansByModel.req(
                 self,
                 datacards=ggf_cards,
@@ -148,14 +172,14 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.), ("kt", -5., 5.),),
             )
 
-        if not self.no_significance_scan:
+        if self.check_enabled("significance_scan"):
             reqs["significance_scan"] = PlotSignificanceScan.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_multiple_significance_scans:
+        if self.check_enabled("multiple_significance_scans"):
             reqs["multiple_significance_scans"] = PlotMultipleSignificanceScans.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -163,14 +187,14 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -5., 5.),),
             )
 
-        if not self.no_pulls_and_impacts:
+        if self.check_enabled("pulls_and_impacts"):
             reqs["pulls_and_impacts"] = PlotPullsAndImpacts.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
             )
             reqs["pulls_and_impacts"].requires().requires().end_branch = 10
 
-        if not self.no_exclusion_and_bestfit:
+        if self.check_enabled("exclusion_and_bestfit"):
             reqs["exclusion_and_bestfit"] = PlotExclusionAndBestFit.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
@@ -178,20 +202,20 @@ class TestPlots(AnalysisTask):
                 scan_parameters=(("kl", -30., 30., 61),),
             )
 
-        if not self.no_exclusion_and_bestfit_2d:
+        if self.check_enabled("exclusion_and_bestfit_2d"):
             reqs["exclusion_and_bestfit_2d"] = PlotExclusionAndBestFit2D.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
                 scan_parameters=(("kl", -30., 30., 61), ("kt", -6., 9., 31)),
             )
 
-        if not self.no_postfit_s_over_b:
+        if self.check_enabled("postfit_s_over_b"):
             reqs["postfit_s_over_b"] = PlotPostfitSOverB.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
             )
 
-        if not self.no_goodness_of_fit:
+        if self.check_enabled("goodness_of_fit"):
             reqs["goodness_of_fit"] = PlotGoodnessOfFit.req(self,
                 datacards=ggf_cards,
                 pois=("r",),
@@ -199,16 +223,16 @@ class TestPlots(AnalysisTask):
                 toys_per_task=15,
             )
 
-        if not self.no_multiple_goodness_of_fits:
+        if self.check_enabled("multiple_goodness_of_fits"):
             reqs["multiple_goodness_of_fits"] = PlotMultipleGoodnessOfFits.req(self,
                 multi_datacards=multi_cards,
                 datacard_names=multi_cards_names,
                 pois=("r",),
-                toys=300,
-                toys_per_task=15,
+                toys=(300,),
+                toys_per_task=(15,),
             )
 
-        if not self.no_morphing_scales:
+        if self.check_enabled("morphing_scales"):
             reqs["morphing_scales"] = PlotMorphingScales.req(self,
                 hh_model="HHModelPinv.model_default",
                 signal="ggf",
@@ -216,21 +240,21 @@ class TestPlots(AnalysisTask):
                 parameter_values=("kt=1",),
             )
 
-        if not self.no_morphed_discriminant:
+        if self.check_enabled("morphed_discriminant"):
             reqs["morphed_discriminant"] = PlotMorphedDiscriminant.req(self,
                 datacards=ggf_cards[:1],
                 hh_models=test_models,
                 signal="ggf",
-                bins=("ee_HH_ggF",),
+                bins=("ch1",),
                 parameter_values=("kl=1", "kt=1"),
             )
 
-        if not self.no_stat_error_scan:
+        if self.check_enabled("stat_error_scan"):
             reqs["stat_error_scan"] = PlotStatErrorScan.req(self,
                 datacards=ggf_cards[:1],
                 hh_models=test_models,
                 signal="ggf",
-                bins=("ee_HH_ggF",),
+                bins=("ch1",),
                 scan_parameters=(("kl", -20, 20, 81),),
                 parameter_values=("kt=1",),
             )
