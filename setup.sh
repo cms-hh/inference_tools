b8fa6f17fb4b203b74b82b5cb97eb9372576b7b5
Marcel R <github.riga@icloud.com>
2023-12-08 16:01:55 +0100

    CI.

--- setup.sh
+++ setup.sh
@@ -50,7 +50,6 @@ setup() {
     # lang defaults
     [ -z "${LANGUAGE}" ] && export LANGUAGE="en_US.UTF-8"
     [ -z "${LANG}" ] && export LANG="en_US.UTF-8"
-    # [ -z "${LC_ALL}" ] && export LC_ALL="en_US.UTF-8"
 
 
     #
