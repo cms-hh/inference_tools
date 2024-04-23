ff0233c5250ae93882eadc27b4cede575612439c
Fabio Monti <fabio.monti@cern.ch>
2024-04-23 15:33:38 +0200

    Add projections campaigns

--- dhi/config.py
+++ dhi/config.py
@@ -140,6 +140,8 @@ campaign_labels = DotDict({
     "2017": "41.5 fb^{-1} (2017, 13 TeV)",
     "2018": "59.8 fb^{-1} (2018, 13 TeV)",
     "run2": "138 fb^{-1} (13 TeV)",
+    "projections3000": "3000 fb^{-1} (14 TeV)",
+    "projections": "(14 TeV)",
 })
 for c, c_label in list(campaign_labels.items()):
     for b, b_label in br_hh_names.items():
