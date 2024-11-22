c6e7d3c79e96daa7971318e2c775c15fac3f8dda
acarvalh <alexandra.oliveira@cern.ch>
2024-11-22 15:28:00 +0100

    removing spurious scenarios

--- dhi/scripts/getScenario_comb.py
+++ dhi/scripts/getScenario_comb.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python
 info = {
-    'S1': {
-      'scale_args': [
-      ]
-    },
-    'S1DBG': {
-      'scale_args': [
-            r"""--X-nuisance-group-function 'pBTag' '1.0'""",
-            r"""--X-nuisance-group-function 'pEleID' '1.0'""",
-            r"""--X-nuisance-group-function 'pMuonID' '1.0'""",
-            r"""--X-nuisance-group-function 'pPhotonID' '1.0'""",
-            r"""--X-nuisance-group-function 'pTauID' '1.0'""",
-            r"""--X-nuisance-group-function 'pScaleJ' '1.0'""",
-            r"""--X-nuisance-group-function 'pResJ' '1.0'""",
-            r"""--X-nuisance-group-function 'pScaleMet' '1.0'""",
-            r"""--X-nuisance-group-function 'pLumi' '1.0'""",
-            r"""--X-nuisance-group-function 'pOther' '1.0'""",
-            r"""--X-nuisance-group-function 'sigTheory' '1.0'"""
-      ]
-    },
     'S2': {
       'scale_args': [
           #from 4b boosted
@@ -52,30 +33,6 @@ info = {
             r"""--X-nuisance-group-function 'bkgTheory' '0.5'"""
       ]
     },
-    'S2NF': {
-      'scale_args': [
-            r"""--X-nuisance-group-function 'pBTag' 'expr::scaleBTag("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pBTagStat' 'expr::scaleBTagStat("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pEleID' 'expr::scaleEleID("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pMuonID' 'expr::scaleMuonID("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pLepFakes' 'expr::scaleLepFakes("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pPhotonID' 'expr::scalePhotonID("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pTauID' '1.0'""",
-            r"""--X-nuisance-group-function 'pScaleJ' 'expr::scaleScaleJ("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleJAbs' 'expr::scaleScaleJAbs("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleJFlav' 'expr::scaleScaleJFlav("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleJPileup' '1.0'""",
-            r"""--X-nuisance-group-function 'pScaleJRel' 'expr::scaleScaleJRel("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleJTime' 'expr::scaleScaleJTime("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleJMethod' 'expr::scaleScaleJMethod("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pResJ' 'expr::scaleResJ("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pScaleMet' 'expr::scaleScaleMet("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'pLumi' '0.6'""",
-            r"""--X-nuisance-group-function 'pOther' 'expr::scaleOther("1/sqrt(@0)",lumiscale[1])'""",
-            r"""--X-nuisance-group-function 'sigTheory' '0.5'""",
-            r"""--X-nuisance-group-function 'bkgTheory' '0.5'"""
-      ]
-    }
 }
 
 def GetOpts(scenario):
